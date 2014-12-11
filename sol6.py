73c340b1c15f5fc3049428eec5e6b6d5d15a7161
Posquit0.BJ <posquit0.bj@gmail.com>
2014-12-11 11:55:49 +0000

    solbed problem 6

---
+++ sol6.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# encoding: utf-8
+import StringIO
+import requests
+import zipfile
+import re
+
+ZIP_URL = "http://www.pythonchallenge.com/pc/def/channel.zip"
+NEXT_REGEX = r"nothing is (\d+)"
+BASE_FILENAME = "%s.txt"
+START = 90052
+
+def main():
+    data = requests.get(ZIP_URL).content
+    data = StringIO.StringIO(data)
+    file = zipfile.ZipFile(data)
+    next = START
+    message = str()
+
+    while True:
+        filename = BASE_FILENAME % next
+        message += file.getinfo(filename).comment
+        data = file.read(filename)
+        found = re.findall(NEXT_REGEX, data)
+        if found:
+            next = found[0]
+        else:
+            print data
+            print message
+            break
+
+if __name__ == '__main__':
+    main()
